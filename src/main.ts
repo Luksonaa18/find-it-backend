e2dfeded456eb47fa231f1526fef024d6fb95da0
Luksonaa_Dev <lukazhozhadze53@gmail.com>
2026-01-10 20:28:38 +0400

    ur

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-   app.useGlobalPipes(
+
+  app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,            // remove extra fields
+      whitelist: true, // remove extra fields
       forbidNonWhitelisted: true, // throw error if extra fields sent
-      transform: true,            // auto-transform types
+      transform: true, // auto-transform types
     }),
   );
-   app.enableCors({
-    origin: 'findit-production-7c39.up.railway.app',
+  app.enableCors({
+    origin: 'https://findit-production-7c39.up.railway.app',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
